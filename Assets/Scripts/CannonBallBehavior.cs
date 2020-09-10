d039b0fc4c89cb85d53e6822b29cbf33afa4f0f1
Bri <brionnafranklin@yahoo.com>
2020-09-10 17:01:54 -0500

    Made chair collisions
    Did the Collision Shape Hierarchies Tutorial

---
+++ Assets/Scripts/CannonBallBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CannonBallBehavior : MonoBehaviour
+{
+    public float forceOnFire = 300;
+    private bool _canFire = true;
+    Rigidbody _rigidbody = null;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown && _canFire)
+        {
+            _rigidbody.isKinematic = false;
+            _rigidbody.AddForce(transform.forward * forceOnFire);
+            _canFire = false;
+        }
+    }
+}
