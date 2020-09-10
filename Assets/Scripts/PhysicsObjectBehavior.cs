7636347c15281ee4b4092564736dc64212621a12
Bri <brionnafranklin@yahoo.com>
2020-09-10 12:57:01 -0500

    created PhysicsObjectBehavoir for asleepMaterial
    Created a cube that changes color when asleep (when no physics is acting on it)

---
+++ Assets/Scripts/PhysicsObjectBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PhysicsObjectBehavior : MonoBehaviour
+{
+    public Material awakeMaterial = null;
+    public Material asleepMaterial = null;
+    private Rigidbody _rigidbody = null;
+    bool wasAsleep = false;
+    // Use this for initialization
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    private void FixedUpdate()
+    {
+        if (_rigidbody.IsSleeping() && !wasAsleep && asleepMaterial != null)
+        {
+            wasAsleep = true;
+            GetComponent<MeshRenderer>().material = asleepMaterial;
+        }
+        if (!_rigidbody.IsSleeping() && wasAsleep && awakeMaterial != null)
+        {
+            wasAsleep = false;
+            GetComponent<MeshRenderer>().material = awakeMaterial;
+        }
+    }
+}
+
